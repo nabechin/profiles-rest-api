657c753a0516d41ae77387df092d0fd3701af9b3
nabenabe0258 <nabenabe0258@gmail.com>
2020-02-23 17:43:26 +0900

    check authentication system and google chrome modeheader to send token to modify user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,7 +8,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     serializer_class = serializer.HelloSerializer
@@ -78,3 +79,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
